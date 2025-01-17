9586cc9a0d34c3902a0281af740102798e36b93c
varworkshop <var.workshop.cvpr@gmail.com>
2025-01-17 18:48:21 +0000

    Deploying to gh-pages from @ varworkshop/varworkshop.github.io@0f9f59dde93af62e0a15edd3d127457b8150659d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -434,8 +434,8 @@ ninja.data = [{
     },{id: "news-the-website-is-now-live",
           title: 'The website is now live.',
           description: "",
-          section: "News",},{id: "projects-ani-kembhavi",
-          title: 'Ani Kembhavi',
+          section: "News",},{id: "projects-avi-kembhavi",
+          title: 'Avi Kembhavi',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
