6e0df9a8656751d00054f5b793f730de2ac18370
varworkshop <var.workshop.cvpr@gmail.com>
2025-01-17 18:27:29 +0000

    Deploying to gh-pages from @ varworkshop/varworkshop.github.io@c078fc0dfc1263e7b1e2aa456a950df514cee090 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,24 +448,24 @@ ninja.data = [{
     },{id: "news-the-website-is-now-live",
           title: 'The website is now live.',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-avi-kembhavi",
+          title: 'Avi Kembhavi',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-kate-saenko",
+          title: 'Kate Saenko',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-marc-pollefeys",
+          title: 'Marc Pollefeys',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-mike-shou",
+          title: 'Mike Shou',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
