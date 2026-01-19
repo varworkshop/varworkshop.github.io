dbd638708fe0dacb4f9928cebcf5c09a5fa27974
varworkshop <var.workshop.cvpr@gmail.com>
2026-01-19 04:03:14 +0000

    Deploying to gh-pages from @ varworkshop/varworkshop.github.io@4eb1a80a56f71e5b74e6f748f2babb5912645f77 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/schedule/";
           },
+        },{id: "nav-program-and-papers",
+          title: "Program and Papers",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/schedule/";
+          },
         },{id: "nav-calls",
           title: "Calls",
           description: "",
